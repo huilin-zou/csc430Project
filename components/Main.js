d6f4e2c8d4fb16419f39637e9a39c55fa6e95249
Mohammad Malik <76458022+mmalik36@users.noreply.github.com>
2022-11-02 14:50:12 -0400

    React + HTML/CSS Files Showcasing Front Page

---
+++ components/Main.js
@@ -0,0 +1,33 @@
+import React from "react"
+
+export default function Main() {
+
+    return (
+        <main>
+            <div className="container">
+                <h1 className="logo">Bumble</h1>
+                <h1 className="logo2">Bee</h1>
+            </div>
+            <div id="wrapper">
+                <p>
+                    <a href='orderNow.html' class="orderButton">ORDER NOW</a>
+                </p>
+            </div>
+            <div>
+                <img className="s-image" src="./sushi.png"></img>
+                <p class="bigBubble">Japanese</p>
+                <p class="bigBubble2"> Restaurant</p>
+            </div>
+            <div>
+                <p className="wordBubble">We try to show you all the variety of Japanese cuisine and its current trends.</p>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <a href='dineIn.html' class="choiceButton">DINE IN</a>
+                <a href='takeOut.html' class="choiceButton">TAKE OUT</a>
+            </div>
+
+        </main>
+    )
+}
