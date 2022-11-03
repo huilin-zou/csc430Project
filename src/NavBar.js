c5053d1a0e9d0c919b2b5d28f1b610c4962ad9cd
Mohammad Malik <76458022+mmalik36@users.noreply.github.com>
2022-11-02 21:48:17 -0400

    Add files via upload

---
+++ src/NavBar.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav className="navigation">
+            <div className="navigation-menu">
+                <ul>
+                    <li>
+                        <a href="/home">Home</a>
+                    </li>
+                    <li>
+                        <a href="/about">About Us</a>
+                    </li>
+                    <li>
+                        <a href="/contact">Menu</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
