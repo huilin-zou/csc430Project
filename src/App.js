8fb77d4e33aea1741b13a85c7c1adefc3087f791
Mohammad Malik <76458022+mmalik36@users.noreply.github.com>
2022-11-02 21:37:40 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './style.css'
+import Navbar from "./NavBar.js";
+import Main from "./Main.js";
+
+function App() {
+  return (
+        <div>
+          <Navbar />
+          <Main />
+        </div>
+  );
+}
+
+export default App;
